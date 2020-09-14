d4fafd04475f017db48a823498f42424d285f2a7
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-09-14 15:48:54 +0100

    🛠 setup vars env

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+      },
+    ],
+  ],
 };
---
+++ template/types/env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const API: string;
+}
