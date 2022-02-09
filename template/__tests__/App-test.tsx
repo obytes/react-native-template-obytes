4c97aa37a544f58261fcfdd11cfcc5b639d51966
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2022-02-09 11:29:54 +0100

    test: fix missed App component

--- template/__tests__/App-test.tsx
+++ template/__tests__/App-test.tsx
@@ -4,10 +4,12 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {View} from 'react-native';
+
+const App = () => <View />;
 
 it('renders correctly', () => {
   renderer.create(<App />);
