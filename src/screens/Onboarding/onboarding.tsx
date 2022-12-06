16e0d4d7d0490cc15faa9c60867daf4880675f54
0x10ayoub <ayoubkhourbach@gmail.com>
2022-12-07 00:13:45 +0100

    fix: change onboarding Message

--- src/screens/Onboarding/onboarding.tsx
+++ src/screens/Onboarding/onboarding.tsx
@@ -5,8 +5,18 @@ import { Button, Text, View } from '@/ui';
 export const Onboarding = () => {
   return (
     <View className="flex justify-center items-center  h-full">
-      <Text className="text-2xl">Hi There</Text>
-      <Button label="Let's Get Started" onPress={() => setOnboarding()} />
+      <Text className="text-lg mb-2">
+        The right way to build your mobile app
+      </Text>
+      <Text className="text-5xl my-3">Obytes Starter</Text>
+      <Text className="text-2xl text-left my-1">Beginner friendly</Text>
+      <Text className="text-2xl text-left my-1">Production-ready</Text>
+      <Text className="text-2xl text-left my-1">
+        Minimal code and dependencies
+      </Text>
+      <View className="mt-6">
+        <Button label="Let's Get Started " onPress={() => setOnboarding()} />
+      </View>
     </View>
   );
 };
