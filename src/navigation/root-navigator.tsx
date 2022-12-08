33c75091274205d250f770eb447ad0b3ab3fe90a
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2022-12-08 14:03:05 +0100

    refactor: refactor isFirstTime user logic

--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -3,25 +3,25 @@ import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
 
 import { useAuth } from '@/core';
-import { useOnboarding } from '@/core/onboarding';
+import { useIsFirstTime } from '@/core/hooks';
+import { Onboarding } from '@/screens';
 
 import { AuthNavigator } from './auth-navigator';
 import { NavigationContainer } from './navigation-container';
-import { OnboardingNavigator } from './onboarding-navigator';
 import { TabNavigator } from './tab-navigator';
 const Stack = createNativeStackNavigator();
 
 export const Root = () => {
   const status = useAuth((state) => state.status);
-  const onboardingStatus = useOnboarding((state) => state.status);
+  const [isFirstTime] = useIsFirstTime();
   const hideSplash = React.useCallback(async () => {
     await SplashScreen.hideAsync();
   }, []);
   useEffect(() => {
-    if (onboardingStatus !== 'idle' && status !== 'idle') {
+    if (status !== 'idle') {
       hideSplash();
     }
-  }, [hideSplash, onboardingStatus, status]);
+  }, [hideSplash, status]);
 
   return (
     <Stack.Navigator
@@ -31,15 +31,16 @@ export const Root = () => {
         animation: 'none',
       }}
     >
-      {onboardingStatus === 'onboardingEnabled' ? (
-        <Stack.Screen
-          name="OnboardingNavigator"
-          component={OnboardingNavigator}
-        />
-      ) : status === 'signOut' ? (
-        <Stack.Screen name="Auth" component={AuthNavigator} />
+      {isFirstTime ? (
+        <Stack.Screen name="Onboarding" component={Onboarding} />
       ) : (
-        <Stack.Screen name="App" component={TabNavigator} />
+        <Stack.Group>
+          {status === 'signOut' ? (
+            <Stack.Screen name="Auth" component={AuthNavigator} />
+          ) : (
+            <Stack.Screen name="App" component={TabNavigator} />
+          )}
+        </Stack.Group>
       )}
     </Stack.Navigator>
   );
--- src/screens/onboarding/onboarding.tsx
+++ src/screens/onboarding/onboarding.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 
-import { disableOnboarding } from '@/core/onboarding';
-import { Button, Text, View } from '@/ui';
+import { useIsFirstTime } from '@/core/hooks';
+import { Button, Image, SafeAreaView, Text, View } from '@/ui';
 export const Onboarding = () => {
+  // TODO: disable this rule for vars with underscore
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const [_, setIsFirstTime] = useIsFirstTime();
   return (
-    <View className="flex h-full items-center  justify-center">
-      <Text className="mb-2 text-lg">
-        The right way to build your mobile app
-      </Text>
-      <Text className="my-3 text-5xl">Obytes Starter</Text>
-      <Text className="my-1 text-left text-2xl">Beginner friendly</Text>
-      <Text className="my-1 text-left text-2xl">Production-ready</Text>
-      <Text className="my-1 text-left text-2xl">
-        Minimal code and dependencies
-      </Text>
-      <View className="mt-6">
+    <View className="flex h-full items-center  justify-center bg-white">
+      <Image className="w-full flex-1" source={require('./cover.png')} />
+
+      <View className="justify-end ">
+        <Text className="my-3 text-5xl font-bold text-center">
+          Obytes Starter
+        </Text>
+        <Text className="mb-2 text-lg text-center text-gray-600">
+          The right way to build your mobile app
+        </Text>
+
+        <Text className="my-1 text-left text-lg pt-6">
+          🚀 Production-ready{' '}
+        </Text>
+        <Text className="my-1 text-left text-lg">
+          🥷 Developer experience + Productivity
+        </Text>
+        <Text className="my-1 text-left text-lg">
+          🧩 Minimal code and dependencies
+        </Text>
+        <Text className="my-1 text-left text-lg">
+          💪 well maintained third-party libraries
+        </Text>
+      </View>
+      <SafeAreaView className="mt-6">
         <Button
           label="Let's Get Started "
           onPress={() => {
-            disableOnboarding();
+            setIsFirstTime(false);
           }}
         />
-      </View>
+      </SafeAreaView>
     </View>
   );
 };
