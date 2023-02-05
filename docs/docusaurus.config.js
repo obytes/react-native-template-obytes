7afd644e661c64c49d408d4ba1eadd44a8369fd8
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-02-05 18:55:36 +0100

    docs: fix google tracking config for docs website

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-        googleAnalytics: {
+        gtag: {
           trackingID: "G-GQ45JJD1JC",
           anonymizeIP: true,
         },
