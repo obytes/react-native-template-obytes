1af76d792cf017fe3d5c627095478a47ae022a43
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-02-20 23:08:08 +0100

    fix: fix flashlist  not working with nativewind

--- src/ui/core/list/index.tsx
+++ src/ui/core/list/index.tsx
@@ -1,8 +1,4 @@
 import { FlashList as NFlashList } from '@shopify/flash-list';
-import { styled } from 'nativewind';
 
 export * from './empty-list';
-
-export const List = styled(NFlashList, {
-  classProps: ['contentContainerStyle', 'className'],
-});
+export const List = NFlashList;
