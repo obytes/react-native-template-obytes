cc93f82bead101cc1dca0cae607a91db846c9e31
ayoub khourbach <ayoubkhourbach@gmail.com>
2023-05-02 13:37:21 +0100

    fix: unable to find a specification

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
-module.exports = getDefaultConfig(__dirname);
+// config for expo, with MetroSymlinkResolver this works with pnpm monorepo
+const projectRoot = __dirname;
+const config = getDefaultConfig(projectRoot);
+config.resolver.resolveRequest = MetroSymlinksResolver();
+module.exports = config;
