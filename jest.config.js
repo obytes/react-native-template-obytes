82215ffb67dd60bf257ee5544c805c2356698830
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-07-20 01:31:06 +0100

    fix: update report action

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   transformIgnorePatterns: [
     `node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))`,
   ],
-  coverageReporters: ['text', 'clover'],
+  coverageReporters: ['text', 'json-summary'],
   reporters: [['github-actions', { silent: false }], 'summary'],
   coverageDirectory: '<rootDir>/reports',
   moduleNameMapper: {
