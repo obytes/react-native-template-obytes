61225d061a4042cd99a42bcdbc5469f8af1925a2
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-07-20 01:08:56 +0100

    fix: use github reporter

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
     `node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))`,
   ],
   coverageReporters: ['text', 'clover'],
+  reporters: [['github-actions', { silent: false }], 'summary'],
   coverageDirectory: '<rootDir>/reports',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
