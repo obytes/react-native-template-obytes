54c0a7ef3d1498c1f4dacc37746197495991293b
Elena Bitire <elena@obytes.com>
2024-02-14 17:53:17 +0200

    fix: fix input & select components

--- src/app/(app)/style.tsx
+++ src/app/(app)/style.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Buttons } from '@/components/buttons';
+import { Inputs } from '@/components/inputs';
 import { Typography } from '@/components/typography';
 import { FocusAwareStatusBar, SafeAreaView, ScrollView } from '@/ui';
 
@@ -11,8 +12,8 @@ export default function Style() {
       <ScrollView>
         <SafeAreaView className="flex-1 px-4">
           <Typography />
-          {/* <ColorVariants />
-          <InputVariants /> */}
+          {/* <ColorVariants /> */}
+          <Inputs />
           <Buttons />
         </SafeAreaView>
       </ScrollView>
