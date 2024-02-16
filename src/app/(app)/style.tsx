a02031e74138434ea68ccc2ed176aba4c912d527
Elena Bitire <elena@obytes.com>
2024-02-16 13:41:52 +0200

    fix: fix color variants

--- src/app/(app)/style.tsx
+++ src/app/(app)/style.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Buttons } from '@/components/buttons';
+import { Colors } from '@/components/colors';
 import { Inputs } from '@/components/inputs';
 import { Typography } from '@/components/typography';
 import { FocusAwareStatusBar, SafeAreaView, ScrollView } from '@/ui';
@@ -12,7 +13,7 @@ export default function Style() {
       <ScrollView>
         <SafeAreaView className="flex-1 px-4">
           <Typography />
-          {/* <ColorVariants /> */}
+          <Colors />
           <Inputs />
           <Buttons />
         </SafeAreaView>
