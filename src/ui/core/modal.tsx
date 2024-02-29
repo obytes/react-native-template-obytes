ee8b0bcdea682669c0cfa0ff9ce1252adb42dde1
Elena Bitire <elena@obytes.com>
2024-02-29 11:43:53 +0200

    fix: small fixes

--- src/ui/core/modal.tsx
+++ src/ui/core/modal.tsx
@@ -87,7 +87,7 @@ export const Modal = React.forwardRef(
     const renderHandleComponent = React.useCallback(
       () => (
         <>
-          <View className="mt-2 h-1 w-12 self-center rounded-lg bg-gray-400 dark:bg-gray-700" />
+          <View className="mb-6 mt-2 h-1 w-12 self-center rounded-lg bg-gray-400 dark:bg-gray-700" />
           <ModalHeader title={title} dismiss={modal.dismiss} />
         </>
       ),
