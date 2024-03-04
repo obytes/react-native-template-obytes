e8fa1ecd25427cdb425039d39ff2b91b1a58068a
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-03-04 11:50:37 +0100

    fix(docs): fix code snippets layout

--- docs/ec.config.mjs
+++ docs/ec.config.mjs
@@ -4,6 +4,6 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   // Example: Using a custom plugin (which makes this `ec.config.mjs` file necessary)
-  plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+  // plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
   // ... any other options you want to configure
 };
