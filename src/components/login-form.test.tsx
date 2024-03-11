71a7ae14e9996a461634a7606eb8946812de90bd
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-03-11 15:29:05 +0000

    feat: use screen api for unit tests

--- src/components/login-form.test.tsx
+++ src/components/login-form.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { cleanup, fireEvent, render, waitFor } from '@/core/test-utils';
+import { cleanup, fireEvent, render, screen, waitFor } from '@/core/test-utils';
 
 import type { LoginFormProps } from './login-form';
 import { LoginForm } from './login-form';
@@ -11,43 +11,41 @@ const onSubmitMock: jest.Mock<LoginFormProps['onSubmit']> = jest.fn();
 
 describe('LoginForm Form ', () => {
   it('renders correctly', async () => {
-    const { findByText } = render(<LoginForm />);
-    expect(await findByText(/Sign in/i)).not.toBeNull();
+    render(<LoginForm />);
+    expect(await screen.findByText(/Sign in/i)).not.toBeNull();
   });
 
   it('should display required error when values are empty', async () => {
-    const { getByText, findByText, queryByText, getByTestId } = render(
-      <LoginForm />
-    );
+    render(<LoginForm />);
 
-    const button = getByTestId('login-button');
-    expect(queryByText(/Email is required/i)).toBeNull();
+    const button = screen.getByTestId('login-button');
+    expect(screen.queryByText(/Email is required/i)).toBeNull();
     fireEvent.press(button);
-    expect(await findByText(/Email is required/i)).not.toBeNull();
-    expect(getByText(/Password is required/i)).not.toBeNull();
+    expect(await screen.findByText(/Email is required/i)).not.toBeNull();
+    expect(screen.getByText(/Password is required/i)).not.toBeNull();
   });
 
   it('should display matching error when email is invalid', async () => {
-    const { getByTestId, findByText, queryByText } = render(<LoginForm />);
+    render(<LoginForm />);
 
-    const button = getByTestId('login-button');
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
+    const button = screen.getByTestId('login-button');
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
 
     fireEvent.changeText(emailInput, 'yyyyy');
     fireEvent.changeText(passwordInput, 'test');
     fireEvent.press(button);
 
-    expect(queryByText(/Email is required/i)).toBeNull();
-    expect(await findByText(/Invalid Email Format/i)).not.toBeNull();
+    expect(screen.queryByText(/Email is required/i)).toBeNull();
+    expect(await screen.findByText(/Invalid Email Format/i)).not.toBeNull();
   });
 
   it('Should call LoginForm with correct values when values are valid', async () => {
-    const { getByTestId } = render(<LoginForm onSubmit={onSubmitMock} />);
+    render(<LoginForm onSubmit={onSubmitMock} />);
 
-    const button = getByTestId('login-button');
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
+    const button = screen.getByTestId('login-button');
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
 
     fireEvent.changeText(emailInput, 'youssef@gmail.com');
     fireEvent.changeText(passwordInput, 'password');
@@ -56,7 +54,7 @@ describe('LoginForm Form ', () => {
       expect(onSubmitMock).toHaveBeenCalledTimes(1);
     });
     // undefined because we don't use second argument of the  SubmitHandler
-    expect(onSubmitMock).toBeCalledWith(
+    expect(onSubmitMock).toHaveBeenCalledWith(
       {
         email: 'youssef@gmail.com',
         password: 'password',
