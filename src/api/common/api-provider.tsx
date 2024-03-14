d330b076e60d4396da05745c8878f2b92ec1373b
Elena Bitire <elena@obytes.com>
2024-03-14 17:29:20 +0200

    feat: add React Query dev tools plugin

--- src/api/common/api-provider.tsx
+++ src/api/common/api-provider.tsx
@@ -1,9 +1,11 @@
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as React from 'react';
 
 export const queryClient = new QueryClient();
 
 export function APIProvider({ children }: { children: React.ReactNode }) {
+  useReactQueryDevTools(queryClient);
   return (
     // Provide the client to your App
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
