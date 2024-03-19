10d816cd3888d30378a9cad2e59126151b4ab165
Elena Bitire <elena@obytes.com>
2024-03-19 21:18:26 +0200

    fix: force the app to LTR

--- src/core/i18n/index.tsx
+++ src/core/i18n/index.tsx
@@ -1,4 +1,3 @@
-import { locale } from 'expo-localization';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { I18nManager } from 'react-native';
@@ -9,7 +8,7 @@ export * from './utils';
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: getLanguage() || locale,
+  lng: getLanguage() || 'en',
   fallbackLng: 'en',
   compatibilityJSON: 'v3', // By default React Native projects does not support Intl
 
