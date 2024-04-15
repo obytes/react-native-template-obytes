1aa19a001e5da2127589291efd2cf3e7f7b4bd98
Elena Bitire <elena@obytes.com>
2024-04-15 13:41:30 +0300

    chore: switch font from Jakarta to Inter

--- src/ui/text.tsx
+++ src/ui/text.tsx
@@ -21,7 +21,7 @@ export const Text = ({
   const textStyle = React.useMemo(
     () =>
       twMerge(
-        'text-base text-black  dark:text-white  font-jakarta font-normal',
+        'text-base text-black  dark:text-white font-inter font-normal',
         className
       ),
     [className]
