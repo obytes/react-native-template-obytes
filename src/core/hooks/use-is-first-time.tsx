b6cd8e03b44b1c083b4a7addf21061110b38452e
Elena Bitire <elena@obytes.com>
2024-04-25 09:46:19 +0300

    chore: add platform specific storage handling for useIsFirstTime hook

--- src/core/hooks/use-is-first-time.tsx
+++ src/core/hooks/use-is-first-time.tsx
@@ -1,11 +1,16 @@
+import { Platform } from 'react-native';
 import { useMMKVBoolean } from 'react-native-mmkv';
 
 import { storage } from '../storage';
+import { webStorage } from '../storage.web';
 
 const IS_FIRST_TIME = 'IS_FIRST_TIME';
 
 export const useIsFirstTime = () => {
-  const [isFirstTime, setIsFirstTime] = useMMKVBoolean(IS_FIRST_TIME, storage);
+  const [isFirstTime, setIsFirstTime] = useMMKVBoolean(
+    IS_FIRST_TIME,
+    Platform.OS === 'web' ? webStorage : storage
+  );
   if (isFirstTime === undefined) {
     return [true, setIsFirstTime] as const;
   }
