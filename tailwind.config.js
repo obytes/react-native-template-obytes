a6218e6ba9ff6d2f1fe744cad84f40a087961f6d
Hamza El Mekkoudi <hamza.mekkoudi@gmail.com>
2024-05-21 17:10:02 +0100

    fix: bring back darkmode property in taiwlind config file to fix dark mode in web

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
