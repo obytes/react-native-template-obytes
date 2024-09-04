rootstrap/react-native-template

feat: add qa env

env.js
@@ -70,7 +70,7 @@ const withEnvSuffix = (name) => {
  */
 
 const client = z.object({
-  APP_ENV: z.enum(['development', 'staging', 'production']),
+  APP_ENV: z.enum(['development', 'staging', 'production', 'qa']),
   NAME: z.string(),
   SCHEME: z.string(),
   BUNDLE_ID: z.string(),

obytes/react-native-template-obytes

env.js
@@ -107,7 +107,7 @@ const parseBoolean = (/** @type {string | undefined} */ value) =>
   value ? value === 'true' : undefined;
 
 const clientEnvSchema = z.object({
-  APP_ENV: z.enum(['development', 'staging', 'production']),
+  APP_ENV: z.enum(['development', 'production', 'qa', 'staging']),
   NAME: z.string(),
   SCHEME: z.string(),
   BUNDLE_ID: z.string(),