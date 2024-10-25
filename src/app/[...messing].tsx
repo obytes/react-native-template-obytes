783474b7c2b0dfe7015156023ba4bb6830f0038f
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-10-25 11:15:22 +0100

    feat: add missed routes

---
+++ src/app/[...messing].tsx
@@ -0,0 +1,20 @@
+import { Link, Stack } from 'expo-router';
+
+import { Text, View } from '@/ui';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View className="flex-1 items-center justify-center p-4">
+        <Text className="mb-4 text-2xl font-bold">
+          This screen doesn't exist.
+        </Text>
+
+        <Link href="/" className="mt-4">
+          <Text className="text-blue-500 underline">Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
