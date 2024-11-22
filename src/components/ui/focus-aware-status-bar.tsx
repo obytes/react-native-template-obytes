fbb52ca85ded7cebc9146b8748275772cc82d1a2
hamzamekk <hamza.mekkoudi@gmail.com>
2024-11-23 00:53:20 +0100

    fix: fix focuse aware statue bar crash in web

--- src/components/ui/focus-aware-status-bar.tsx
+++ src/components/ui/focus-aware-status-bar.tsx
@@ -1,12 +1,15 @@
 import { useIsFocused } from '@react-navigation/native';
 import { useColorScheme } from 'nativewind';
 import * as React from 'react';
+import { Platform } from 'react-native';
 import { SystemBars } from 'react-native-edge-to-edge';
 
 type Props = { hidden?: boolean };
 export const FocusAwareStatusBar = ({ hidden = false }: Props) => {
   const isFocused = useIsFocused();
   const { colorScheme } = useColorScheme();
+
+  if (Platform.OS === 'web') return null;
 
   return isFocused ? <SystemBars style={colorScheme} hidden={hidden} /> : null;
 };
