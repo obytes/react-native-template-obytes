1b6b1f7be22f8c352d21dab13253a4da81811696
Todd Fisher <toddwashere@hotmail.com>
2025-02-01 15:15:35 -0700

    comment out certain estlint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,8 @@ module.exports = {
         ignore: ['/android', '/ios'],
       },
     ],
-    'max-params': ['error', 3], // Limit the number of parameters in a function to use object instead
-    'max-lines-per-function': ['error', 70],
+    // 'max-params': ['error', 3], // Limit the number of parameters in a function to use object instead
+    // 'max-lines-per-function': ['error', 70],
     'react/display-name': 'off',
     'react/no-inline-styles': 'off',
     'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
