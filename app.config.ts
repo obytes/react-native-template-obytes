c3122946f879db38dd8bb6aa61e9270d13b3f162
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2025-02-18 16:59:24 +0100

    fix: avoid the export complaince warning on the app store

--- app.config.ts
+++ app.config.ts
@@ -39,6 +39,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: Env.BUNDLE_ID,
+    config: {
+      usesNonExemptEncryption: false, // Avoid the export compliance warning on the app store
+    },
   },
   experiments: {
     typedRoutes: true,
