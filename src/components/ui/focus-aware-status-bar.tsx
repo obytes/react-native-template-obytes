ae94e508b94d8c82e42cbec075ec2720becc72fb
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2025-06-27 17:27:25 +0100

    fix: fix status bar theme based on the selected colorScheme

--- src/components/ui/focus-aware-status-bar.tsx
+++ src/components/ui/focus-aware-status-bar.tsx
@@ -11,5 +11,10 @@ export const FocusAwareStatusBar = ({ hidden = false }: Props) => {
 
   if (Platform.OS === 'web') return null;
 
-  return isFocused ? <SystemBars style={colorScheme} hidden={hidden} /> : null;
+  return isFocused ? (
+    <SystemBars
+      style={colorScheme === 'light' ? 'dark' : 'light'}
+      hidden={hidden}
+    />
+  ) : null;
 };
